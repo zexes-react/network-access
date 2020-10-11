be2200cd2b11811301c3cc2fcdc893c0b51f0851
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-10-11 22:34:41 +0100

    Sending a DELETE Request. closes #6

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -27,6 +27,13 @@ class FullPost extends Component {
         }
     }
 
+    deletePostHandler =() =>{
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if(this.props.id){
@@ -40,7 +47,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className={classes.Edit}>
-                        <button className={classes.Delete}>Delete</button>
+                        <button className={classes.Delete} onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
 
