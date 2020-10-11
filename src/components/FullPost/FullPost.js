8d2293b4d720adb880cffb6301a05d108664c8a9
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-10-11 21:20:43 +0100

    Make a Post Selectable (Clickable). closes #3

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -4,17 +4,19 @@ import classes from './FullPost.module.css';
 
 class FullPost extends Component {
     render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className={classes.FullPost}>
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className={classes.Edit}>
-                    <button className={classes.Delete}>Delete</button>
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if(this.props.id){
+            post = (
+                <div className={classes.FullPost}>
+                    <h1>Title</h1>
+                    <p>Content</p>
+                    <div className={classes.Edit}>
+                        <button className={classes.Delete}>Delete</button>
+                    </div>
                 </div>
-            </div>
 
-        );
+            );
+        }
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import classes from './Blog.module.css';
 
 class Blog extends Component {
     state ={
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -34,9 +35,17 @@ class Blog extends Component {
         });
     }
 
+    postSelectedHandler = (id) =>{
+        this.setState({selectedPostId: id})
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>
         });
         return (
             <div>
@@ -44,7 +53,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
