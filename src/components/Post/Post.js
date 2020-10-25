3ac7d4d2e200efaf4fa5b8950e71098d1e06ed0c
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-10-25 21:33:49 +0100

    The "withRouter" HOC & Route Props. closes #19

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import  classes from './Post.module.css';
+import {withRouter} from 'react-router-dom';
 
-const post = (props) => (
-    <article className={classes.Post} onClick={props.clicked}>
-        <h1>{props.title}</h1>
-        <div className="Info">
-            <div className={classes.Author}>{props.author}</div>
-        </div>
-    </article>
-);
+const post = (props) => {
+    console.log(props)
+    return (
+        <article className={classes.Post} onClick={props.clicked}>
+            <h1>{props.title}</h1>
+            <div className="Info">
+                <div className={classes.Author}>{props.author}</div>
+            </div>
+        </article>
+    )
+};
 
-export default post;
+export default withRouter(post);
